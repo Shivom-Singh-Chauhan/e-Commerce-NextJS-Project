a13fbb02893d6ef17ddfe141736217ff4101c20c
Shiv om Singh Chauhan <shivomsingh5124@gmail.com>
2022-07-01 13:26:26 +0530

    Cart Creation, Pin Code , Pages, Product Page Creation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,69 @@
+import Footer from '../components/footer'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
+import {useEffect, useState} from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [cart, setcart] = useState({})
+  const [subtotal, setsubtotal] = useState(0)
+
+  useEffect(() => {
+    console.log("hey im a useeffect from app.js")
+    try{
+      if(localStorage.getItem('cart')){
+        setcart(JSON.parse(localStorage.getItem('cart')))
+       }
+    }catch(error)
+    {console.error(error);
+    localStorage.clear()}
+
+  }, [])
+
+  const saveCart = (myCart)=>{
+    localStorage.setItem("cart", myCart)
+    let subt = 0;
+    let keys = Object.keys(myCart)
+    for(let i=0 ; i<keys.length ; i++)
+    {
+      subt += myCart[keys[i]].price * myCart[keys[i]].qty;
+    }
+    setsubtotal(subt)
+  }
+
+  const addtocart =(itemCode, qty, price, name, size, variant)=>{
+    console.log(itemCode, qty, price, name, size, variant)
+      let newCart = cart;
+      if(itemCode in cart){
+        newCart[itemCode].qty = cart[itemCode].qty + qty;
+      }
+      else
+      {
+        newCart[itemCode] = {qty : 1, price, name, size, variant}
+      }
+      setcart(newCart)
+      saveCart(newCart)
+  }
+
+  const clearCart = ()=>{
+    setcart({})
+    saveCart({})
+  }
+  const removeFromCart =(itemCode, qty, price, name, size, variant)=>{
+    let newCart = cart;
+    if(itemCode in cart){
+      newCart[itemCode].qty = cart[itemCode].qty - qty;
+    }
+    if(newCart[itemCode]["qty"]<=0){
+      delete newCart[itemCode]
+    }
+    setcart(newCart)
+    saveCart(newCart)
+}
+  return <>
+  <Navbar cart={cart} addtocart={addtocart} removeFromCart={removeFromCart} clearCart={clearCart} subtotal={subtotal}/>
+  <Component cart={cart} addtocart={addtocart} removeFromCart={removeFromCart} clearCart={clearCart} subtotal={subtotal}{...pageProps} />
+  <Footer></Footer>
+  </>
 }
 
 export default MyApp
---
+++ pages/api/pincode.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json([201016, 251001, 110001])
+}
